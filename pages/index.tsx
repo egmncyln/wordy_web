d00cddb9c0ccbe06307f9039200228c1b1c0570b
Egemen Ceylan <egemen.ceylan1@gmail.com>
2022-03-01 04:50:37 +0300

    added simple request

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
+import axios from 'axios';
 import { NextPage } from 'next';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Index: NextPage = () => {
-  return (
-    <div>Index</div>
-  );
+  useEffect(() => {
+    axios
+      .get('https://egmncyln-wordy-api.herokuapp.com/logout')
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }, []);
+  return <div>Index</div>;
 };
 
 export default Index;
