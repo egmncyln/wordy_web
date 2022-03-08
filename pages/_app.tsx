016558b3836175ad48d988fef1e3bf3b151d48bf
Egemen Ceylan <egemen.ceylan1@gmail.com>
2022-03-09 01:33:03 +0300

    implemented auth0 provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
 import '../styles/main.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  let redirectUri: string;
+  useEffect(() => { redirectUri = window.location.origin; }, []);
+
   return (
-    <>
+    <Auth0Provider
+      domain="egmncyln.us.auth0.com"
+      clientId="346PbsY9q9TxyZQyfzJhJ5c9PXbWWfm1"
+      redirectUri={redirectUri}
+    >
       <Head>
         <title>Wordy App</title>
         <link rel='icon' href='/favicon.ico' />
@@ -12,7 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         {/* <script src='scripts/event-listeners.js'></script> */}
       </Head>
       <Component {...pageProps} />
-    </>
+    </Auth0Provider>
   );
 }
 
