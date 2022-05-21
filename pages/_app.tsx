94ab411ff4ad497603ea26c961e829a913775273
Egemen Ceylan <egemen.ceylan1@gmail.com>
2022-05-21 23:27:57 +0300

    removed some packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,28 +3,27 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { useEffect } from 'react';
-import ErrorBoundary from '../components/error-boundary';
 
 function MyApp({ Component, pageProps }: AppProps) {
   let redirectUri: string;
-  useEffect(() => { redirectUri = window.location.origin; }, []);
+  useEffect(() => {
+    redirectUri = window.location.origin;
+  }, []);
 
   return (
-    <ErrorBoundary>
-      <Auth0Provider
-        domain="egmncyln.us.auth0.com"
-        clientId="346PbsY9q9TxyZQyfzJhJ5c9PXbWWfm1"
-        redirectUri={redirectUri}
-      >
-        <Head>
-          <title>Wordy App</title>
-          <link rel='icon' href='/favicon.ico' />
-          {/* <script src='scripts/attach-voice.js'></script> */}
-          {/* <script src='scripts/event-listeners.js'></script> */}
-        </Head>
-        <Component {...pageProps} />
-      </Auth0Provider>
-    </ErrorBoundary>
+    <Auth0Provider
+      domain='egmncyln.us.auth0.com'
+      clientId='346PbsY9q9TxyZQyfzJhJ5c9PXbWWfm1'
+      redirectUri={redirectUri}
+    >
+      <Head>
+        <title>Wordy App</title>
+        <link rel='icon' href='/favicon.ico' />
+        {/* <script src='scripts/attach-voice.js'></script> */}
+        {/* <script src='scripts/event-listeners.js'></script> */}
+      </Head>
+      <Component {...pageProps} />
+    </Auth0Provider>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,8 @@
 import axios from 'axios';
 import { NextPage } from 'next';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 const Index: NextPage = () => {
-  useEffect(() => {
-    axios
-      .get('https://egmncyln-wordy-api.herokuapp.com/logout')
-      .then(res => console.log(res))
-      .catch(err => console.log(err));
-  }, []);
   return <div>Index</div>;
 };
 
