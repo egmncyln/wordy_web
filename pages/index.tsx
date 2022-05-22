c34538340c7b7698cf1c9c9c3508a85e316103a4
Egemen Ceylan <egemen.ceylan1@gmail.com>
2022-05-23 01:42:49 +0300

    removed input closint tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,29 +7,23 @@ import Input from '../components/input/Input';
 const Index: NextPage = () => {
   return (
     <Form onSubmit={e => console.log(e)}>
-      <Input className='mb-4' id='email' type='text' label='E-mail' placeholder='E-mail'></Input>
-      <Input className='mb-4' id='name' type='text' label='Name' placeholder='Name'></Input>
-      <Input
-        className='mb-4'
-        id='surname'
-        type='text'
-        label='Surname'
-        placeholder='Surname'
-      ></Input>
+      <Input className='mb-4' id='email' type='text' label='E-mail' placeholder='E-mail' />
+      <Input className='mb-4' id='name' type='text' label='Name' placeholder='Name' />
+      <Input className='mb-4' id='surname' type='text' label='Surname' placeholder='Surname' />
       <Input
         className='mb-4'
         id='password'
         type='password'
         label='Password'
         placeholder='•••••••••'
-      ></Input>
+      />
       <Input
         className='mb-6'
         id='confirm-password'
         type='password'
         label='Confirm password'
         placeholder='•••••••••'
-      ></Input>
+      />
       <Button type='submit' color='default' className='w-full'>
         Register
       </Button>
