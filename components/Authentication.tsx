913029d26b8c7080ce656ed626a21180d81aded5
Egemen Ceylan <egemen.ceylan1@gmail.com>
2022-05-22 12:49:30 +0300

    some improvements

---
+++ components/Authentication.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Authentication() {
+  return (
+    <form>
+      <span className='bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900'>
+        Green
+      </span>
+    </form>
+  );
+}
+
+export default Authentication;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import { NextPage } from 'next';
 import React from 'react';
+import Authentication from '../components/Authentication';
 
 const Index: NextPage = () => {
-  return <div>Index</div>;
+  return (
+    <div>
+      <Authentication />
+    </div>
+  );
 };
 
 export default Index;
