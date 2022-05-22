88122c05fd72c4aa2b9e4ef18c8cddb1da73d62f
Egemen Ceylan <egemen.ceylan1@gmail.com>
2022-05-22 23:03:29 +0300

    tried to fix build error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import { NextPage } from 'next';
-import React from 'react';
+
 import Form from '../components/form/Form';
 import Input from '../components/input/Input';
 
 const Index: NextPage = () => {
   return (
     <Form onSubmit={e => console.log(e)}>
-      <Input></Input>
+      <Input id='id' type='text' label='Label' placeholder='Placeholder'></Input>
       <button type='submit'>Submit</button>
     </Form>
   );
