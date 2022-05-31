97b5cf07036984cda9e70520767c1bd2528f6b35
Egemen Ceylan <egemen.ceylan1@gmail.com>
2022-05-31 22:51:23 +0300

    added auth component

---
+++ components/page/Authentication.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+
+import Anchor from '../web/anchor/Anchor';
+import Button from '../web/button/Button';
+import Form from '../web/form/Form';
+import Input from '../web/input/Input';
+import Small from '../web/small/Small';
+
+enum PageTypes {
+  Login,
+  Signup,
+}
+
+function Authentication() {
+  const [pageType, setPageType] = useState(PageTypes.Login);
+
+  const strPassword = 'Password';
+  const strEmail = 'E-mail';
+  const strName = 'Name';
+  const strSurname = 'Surname';
+  const strPasswordPlaceholder = '•••••••••';
+
+  const strLogin = 'Login';
+  const strSignup = 'Signup';
+
+  const isPageTypeSignup = () => pageType === PageTypes.Signup;
+
+  const formSubmitHandler = e => console.log(e);
+
+  const anchorClickHandler = () =>
+    isPageTypeSignup() ? setPageType(PageTypes.Login) : setPageType(PageTypes.Signup);
+  return (
+    <Form onSubmit={formSubmitHandler}>
+      <Input id={strEmail.toLowerCase()} type='text' label={strEmail} placeholder={strEmail} />
+      {isPageTypeSignup() && (
+        <Input
+          className='mt-4'
+          id={strName.toLowerCase()}
+          type='text'
+          label={strName}
+          placeholder={strName}
+        />
+      )}
+      {isPageTypeSignup() && (
+        <Input
+          className='mt-4'
+          id={strSurname.toLowerCase()}
+          type='text'
+          label={strSurname}
+          placeholder={strSurname}
+        />
+      )}
+      <Input
+        className='mt-4'
+        id={strPassword.toLowerCase()}
+        type='password'
+        label={strPassword}
+        placeholder={strPasswordPlaceholder}
+      />
+      {isPageTypeSignup() && (
+        <Input
+          className='mt-4'
+          id={`confirm-${strPassword.toLowerCase()}`}
+          type='password'
+          label={`Confirm ${strPassword.toLowerCase()}`}
+          placeholder={strPasswordPlaceholder}
+        />
+      )}
+      <Button type='submit' color='default' className='w-full mt-6'>
+        {isPageTypeSignup() ? strSignup : strLogin}
+      </Button>
+      <Small className='block text-center mt-4'>
+        {isPageTypeSignup() ? `Do you already have an account? ` : `Do you have not an account? `}
+        <Anchor onClick={anchorClickHandler}>{isPageTypeSignup() ? strLogin : strSignup}</Anchor>
+      </Small>
+    </Form>
+  );
+}
+
+export default Authentication;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,9 @@
 import { NextPage } from 'next';
-import { useState } from 'react';
 
-import Anchor from '../components/anchor/Anchor';
-import Button from '../components/button/Button';
-import Form from '../components/form/Form';
-import Input from '../components/input/Input';
-import Small from '../components/small/Small';
-
-enum PageTypes {
-  Login,
-  Signup,
-}
+import Authentication from '../components/page/Authentication';
 
 const Index: NextPage = () => {
-  const [pageType, setPageType] = useState(PageTypes.Login);
-
-  const strPassword = 'Password';
-  const strEmail = 'E-mail';
-  const strName = 'Name';
-  const strSurname = 'Surname';
-  const strPasswordPlaceholder = '•••••••••';
-
-  const strLogin = 'Login';
-  const strSignup = 'Signup';
-
-  const isPageTypeSignup = () => pageType === PageTypes.Signup;
-
-  const formSubmitHandler = e => console.log(e);
-
-  const anchorClickHandler = () =>
-    isPageTypeSignup() ? setPageType(PageTypes.Login) : setPageType(PageTypes.Signup);
-
-  return (
-    <Form onSubmit={formSubmitHandler}>
-      <Input id={strEmail.toLowerCase()} type='text' label={strEmail} placeholder={strEmail} />
-      {isPageTypeSignup() && (
-        <Input
-          className='mt-4'
-          id={strName.toLowerCase()}
-          type='text'
-          label={strName}
-          placeholder={strName}
-        />
-      )}
-      {isPageTypeSignup() && (
-        <Input
-          className='mt-4'
-          id={strSurname.toLowerCase()}
-          type='text'
-          label={strSurname}
-          placeholder={strSurname}
-        />
-      )}
-      <Input
-        className='mt-4'
-        id={strPassword.toLowerCase()}
-        type='password'
-        label={strPassword}
-        placeholder={strPasswordPlaceholder}
-      />
-      {isPageTypeSignup() && (
-        <Input
-          className='mt-4'
-          id={`confirm-${strPassword.toLowerCase()}`}
-          type='password'
-          label={`Confirm ${strPassword.toLowerCase()}`}
-          placeholder={strPasswordPlaceholder}
-        />
-      )}
-      <Button type='submit' color='default' className='w-full mt-6'>
-        {isPageTypeSignup() ? strSignup : strLogin}
-      </Button>
-      <Small className='block text-center mt-4'>
-        {isPageTypeSignup() ? `Do you already have an account? ` : `Do you have not an account? `}
-        <Anchor onClick={anchorClickHandler}>{isPageTypeSignup() ? strLogin : strSignup}</Anchor>
-      </Small>
-    </Form>
-  );
+  return <Authentication />;
 };
 
 export default Index;
